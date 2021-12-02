deb70b0195606df6ff98d321c7106dfc218f70ae
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-02 21:13:12 +0700

    feat(repository_comment): add comment repository

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,11 @@
+class CommentRepository {
+  async addComment(addComment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getById(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
