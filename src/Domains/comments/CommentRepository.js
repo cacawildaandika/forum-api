652989744f2ac86c36e3a6f23672e09282477bdd
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-04 21:21:01 +0700

    feat(repository_comment): add function delete comment

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -6,6 +6,10 @@ class CommentRepository {
   async getById(id) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async deleteComment(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = CommentRepository;
