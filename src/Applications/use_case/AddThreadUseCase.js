af8acc227eaad1bb30965efd564eebb4bc3ceda6
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-04 13:28:04 +0700

    refactor(usecase_thread): update use case add thread
    add token validation and decoding payload

--- src/Applications/use_case/AddThreadUseCase.js
+++ src/Applications/use_case/AddThreadUseCase.js
@@ -1,13 +1,28 @@
 const AddThread = require('../../Domains/threads/entities/AddThread');
 
 class AddThreadUseCase {
-  constructor({ threadRepository }) {
+  constructor({ threadRepository, authenticationRepository, authenticationTokenManager }) {
     this._threadRepository = threadRepository;
+    this._authenticationRepository = authenticationRepository;
+    this._authenticationTokenManager = authenticationTokenManager;
   }
 
   async execute(useCasePayload) {
-    const addThread = new AddThread(useCasePayload);
+    this.verifyPayload(useCasePayload);
+    const { title, body, token } = useCasePayload;
+
+    await this._authenticationRepository.checkAvailabilityToken(token);
+    const { id } = await this._authenticationTokenManager
+      .decodePayload(token);
+
+    const addThread = new AddThread({ title, body, owner: id });
     return this._threadRepository.addThread(addThread);
+  }
+
+  verifyPayload({ title, body, token }) {
+    if (!title || !body || !token) {
+      throw new Error('USE_CASE_ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
   }
 }
 
--- src/Applications/use_case/_test/AddThreadUseCase.test.js
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -2,13 +2,34 @@ const AddThreadUseCase = require('../AddThreadUseCase');
 const AddedThread = require('../../../Domains/threads/entities/AddedThread');
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
 const AddThread = require('../../../Domains/threads/entities/AddThread');
+const AuthenticationRepository = require('../../../Domains/authentications/AuthenticationRepository');
+const AuthenticationTokenManager = require('../../security/AuthenticationTokenManager');
 
 describe('AddThreadUseCase', () => {
+  it('should return error when payload is not complete', async () => {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+    const authenticationRepository = new AuthenticationRepository();
+    const authenticationTokenManager = new AuthenticationTokenManager();
+    const addThreadUseCase = new AddThreadUseCase({
+      threadRepository,
+      authenticationRepository,
+      authenticationTokenManager,
+    });
+    const payload = {
+      title: 'title thread',
+    };
+
+    // Action & Assert
+    await expect(addThreadUseCase.execute(payload)).rejects.toThrowError('USE_CASE_ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
   it('should orchestrating add thread function correctly', async () => {
     // Arrange
     const useCasePayload = {
       title: 'Thread title',
       body: 'Thread body',
+      token: 'userToken',
     };
 
     const expectedAddedThread = new AddedThread({
@@ -19,23 +40,28 @@ describe('AddThreadUseCase', () => {
 
     // Add thread dependency
     const mockThreadRepository = new ThreadRepository();
+    const mockAuthenticationRepository = new AuthenticationRepository();
+    const mockAuthenticationTokenManager = new AuthenticationTokenManager();
 
     // Mock add thread function
+    mockAuthenticationRepository.checkAvailabilityToken = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockAuthenticationTokenManager.decodePayload = jest.fn()
+      .mockImplementation(() => Promise.resolve({ id: 'user-123', username: 'andika' }));
     mockThreadRepository.addThread = jest.fn()
       .mockImplementation(() => Promise.resolve(expectedAddedThread));
 
     // Use case instance
     const addThreadUseCase = new AddThreadUseCase({
       threadRepository: mockThreadRepository,
+      authenticationRepository: mockAuthenticationRepository,
+      authenticationTokenManager: mockAuthenticationTokenManager,
     });
 
     // Action
     const addedThread = await addThreadUseCase.execute(useCasePayload);
 
     expect(addedThread).toStrictEqual(expectedAddedThread);
-    expect(mockThreadRepository.addThread).toBeCalledWith(new AddThread({
-      title: useCasePayload.title,
-      body: useCasePayload.body,
-    }));
+    expect(mockThreadRepository.addThread).toBeCalledWith(new AddThread({ title: useCasePayload.title, body: useCasePayload.body, owner: 'user-123' }));
   });
 });
