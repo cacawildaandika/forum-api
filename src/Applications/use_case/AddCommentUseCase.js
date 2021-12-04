f485eb72e0f947c1fa6fa00bf1b29362de9d0209
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-04 13:36:31 +0700

    refactor(usecase_comment): update use case add comment
    add token validation and decoding payload for getting owner id

--- src/Applications/use_case/AddCommentUseCase.js
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -1,16 +1,33 @@
 const AddComment = require('../../Domains/comments/entities/AddComment');
 
 class AddCommentUseCase {
-  constructor({ commentRepository }) {
+  constructor({ commentRepository, authenticationRepository, authenticationTokenManager }) {
     this._commentRepository = commentRepository;
+    this._authenticationRepository = authenticationRepository;
+    this._authenticationTokenManager = authenticationTokenManager;
   }
 
   async execute(useCasePayload) {
+    this.verifyPayload(useCasePayload);
+    const { content, token, thread } = useCasePayload;
+
+    await this._authenticationRepository.checkAvailabilityToken(token);
+    const { id } = await this._authenticationTokenManager
+      .decodePayload(token);
+
     const addComment = new AddComment({
-      content: useCasePayload.content,
+      content,
+      owner: id,
+      thread,
     });
 
     return this._commentRepository.addComment(addComment);
+  }
+
+  verifyPayload({ content, token, thread }) {
+    if (!content || !token || !thread) {
+      throw new Error('USE_CASE_ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
   }
 }
 
--- src/Applications/use_case/_test/AddCommentUseCase.test.js
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -2,12 +2,16 @@ const AddedComment = require('../../../Domains/comments/entities/AddedComment');
 const CommentRepository = require('../../../Domains/comments/CommentRepository');
 const AddCommentUseCase = require('../AddCommentUseCase');
 const AddComment = require('../../../Domains/comments/entities/AddComment');
+const AuthenticationRepository = require('../../../Domains/authentications/AuthenticationRepository');
+const AuthenticationTokenManager = require('../../security/AuthenticationTokenManager');
 
 describe('AddCommentUseCase', () => {
   it('should orchestrating add comment function correctly', async () => {
     // Arrange
     const useCasePayload = {
+      token: 'userToken',
       content: 'Comment should here',
+      thread: 'thread-123',
     };
 
     const expectedAddedComment = new AddedComment({
@@ -18,14 +22,22 @@ describe('AddCommentUseCase', () => {
 
     // Generate Add Comment use case dependency
     const mockCommentRepository = new CommentRepository();
+    const mockAuthenticationRepository = new AuthenticationRepository();
+    const mockAuthenticationTokenManager = new AuthenticationTokenManager();
 
     // Implement mock
+    mockAuthenticationRepository.checkAvailabilityToken = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockAuthenticationTokenManager.decodePayload = jest.fn()
+      .mockImplementation(() => Promise.resolve({ id: 'user-123', username: 'andika' }));
     mockCommentRepository.addComment = jest.fn()
       .mockImplementation(() => Promise.resolve(expectedAddedComment));
 
     // Generate use case instance
     const addCommentUseCase = new AddCommentUseCase({
       commentRepository: mockCommentRepository,
+      authenticationRepository: mockAuthenticationRepository,
+      authenticationTokenManager: mockAuthenticationTokenManager,
     });
 
     // Action
@@ -35,6 +47,8 @@ describe('AddCommentUseCase', () => {
     expect(addedComment).toStrictEqual(expectedAddedComment);
     expect(mockCommentRepository.addComment).toBeCalledWith(new AddComment({
       content: useCasePayload.content,
+      owner: 'user-123',
+      thread: useCasePayload.thread,
     }));
   });
 });
