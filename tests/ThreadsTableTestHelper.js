b98e02e966963c74695ca8f85b6d6f396b3e0c15
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-05 12:48:57 +0700

    feat(repository_thread): add test helper

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,32 @@
+/* istanbul ignore file */
+/* eslint-disable camelcase */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123', title = 'title thread', body = 'body thread', user_id = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
+      values: [id, title, body, user_id],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadsById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
