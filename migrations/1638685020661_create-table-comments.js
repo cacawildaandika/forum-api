d8525012863263d8fc91826ccbffbf2faff74c78
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-05 13:32:50 +0700

    feat(repository_comment): add migration

---
+++ migrations/1638685020661_create-table-comments.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'VARCHAR(255)',
+    },
+    thread_id: {
+      type: 'VARCHAR(255)',
+    },
+    user_id: {
+      type: 'VARCHAR(255)',
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
