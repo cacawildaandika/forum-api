0b7deb5b978797a223dc289fbf7a4cab62764293
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-12-05 12:48:45 +0700

    feat(repository_thread): add migration

---
+++ migrations/1638675507692_create-table-threads.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+    },
+    body: {
+      type: 'VARCHAR(255)',
+    },
+    user_id: {
+      type: 'VARCHAR(255)',
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
