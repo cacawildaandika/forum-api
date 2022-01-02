5e5b4f5c39238847fe06a4c0688d41bf1e702cf8
Caca Wilda Andika <cacawildaandika@gmail.com>
2022-01-02 15:52:54 +0700

    test(thread): add add_thread http test

---
+++ src/Infrastructures/http/_test/thread.test.js
@@ -0,0 +1,111 @@
+const pool = require('../../database/postgres/pool');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const ThreadTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const createServer = require('../createServer');
+const container = require('../../container');
+
+describe('/threads endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
+    await ThreadTableTestHelper.cleanTable();
+  });
+
+  describe('add thread', () => {
+    it('should response 201 and persisted thread', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({});
+      // eslint-disable-next-line no-undef
+      const server = await createServer(container);
+
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'cacawildaandika',
+          password: 'secret',
+          fullname: 'Dicoding Indonesia',
+        },
+      });
+
+      // login user
+      const loginResponse = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: {
+          username: 'cacawildaandika',
+          password: 'secret',
+        },
+      });
+      const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        payload: {
+          title: 'title thread',
+          body: 'body thread',
+        },
+      });
+
+      // Action
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+    });
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // Arrange;
+      await UsersTableTestHelper.addUser({});
+      // eslint-disable-next-line no-undef
+      const server = await createServer(container);
+
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'cacawildaandika',
+          password: 'secret',
+          fullname: 'Dicoding Indonesia',
+        },
+      });
+
+      // login user
+      const loginResponse = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: {
+          username: 'cacawildaandika',
+          password: 'secret',
+        },
+      });
+      const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        payload: {
+          title: 'title thread',
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidsak dapat membuat thread karena properti yang dibutuhkan kurang');
+    });
+  });
+});
