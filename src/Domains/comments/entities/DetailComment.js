1ef2d1fb918a95c847ca8d963691b1882feeba68
Caca Wilda Andika <cacawildaandika@gmail.com>
2022-01-12 00:43:19 +0700

    feat(comment): change like type to int

--- src/Domains/comments/entities/DetailComment.js
+++ src/Domains/comments/entities/DetailComment.js
@@ -12,11 +12,11 @@ class DetailComment {
   verifyPayload({
     id, username, date, content, likeCount,
   }) {
-    if (!id || !username || !date || !content || !likeCount) {
+    if (!id || !username || !date || !content || likeCount === undefined) {
       throw new Error('COMMENT_DETAIL.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof id !== 'string' || typeof username !== 'string' || typeof date !== 'string' || typeof content !== 'string' || typeof likeCount !== 'string') {
+    if (typeof id !== 'string' || typeof username !== 'string' || typeof date !== 'string' || typeof content !== 'string' || Number.isNaN(likeCount)) {
       throw new Error('COMMENT_DETAIL.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
   }
