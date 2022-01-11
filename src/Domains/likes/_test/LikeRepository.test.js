76c80e908fbed8c63bdbd1b01c302df5184d4819
Caca Wilda Andika <cacawildaandika@gmail.com>
2022-01-11 21:39:52 +0700

    test(like): add function get last like user in interface repo

--- src/Domains/likes/_test/LikeRepository.test.js
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -7,5 +7,6 @@ describe('LikeRepository Interface', () => {
 
     // Action and Assert
     await expect(likeRepository.addLike({})).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.getLastLikeUser({})).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
