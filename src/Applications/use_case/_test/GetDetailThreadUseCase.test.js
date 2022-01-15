04d75ef1027f9f59422544d33974057f42531a5e
Caca Wilda Andika <cacawildaandika@gmail.com>
2022-01-16 00:19:28 +0700

    test(detail thread use case): add test mock implementation function

--- src/Applications/use_case/_test/GetDetailThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetDetailThreadUseCase.test.js
@@ -17,8 +17,6 @@ describe('GetDetailThreadUseCase', () => {
       .mockImplementation(() => Promise.reject(new InvariantError('DETAIL_THREAD.NOT_FOUND')));
     mockThreadRepository.verifyDeletedThread = jest.fn()
       .mockImplementation(() => Promise.resolve(false));
-    mockCommentRepository.getByThread = jest.fn()
-      .mockImplementation(() => Promise.resolve([]));
 
     // Generate use case instance
     const getDetailThreadUseCase = new GetDetailThreadUseCase({
@@ -28,50 +26,20 @@ describe('GetDetailThreadUseCase', () => {
 
     // Action
     await expect(getDetailThreadUseCase.execute(id)).rejects.toThrowError('DETAIL_THREAD.NOT_FOUND');
+    expect(mockThreadRepository.verifyDeletedThread).toBeCalledWith(id);
+    expect(mockThreadRepository.getById).toBeCalledWith(id);
   });
 
   it('should return error when thread is deleted', async () => {
     const id = 'thread-123';
-
-    const comments = new DetailComment({
-      id: 'user-abc01',
-      username: 'username',
-      date: '2021-01-01 20:20:10',
-      content: 'content',
-      likeCount: 0,
-    });
-
-    const expectedThread = {
-      id,
-      title: 'title thread',
-      body: 'body thread',
-      date: '2021-01-01 20:20:10',
-      username: 'username',
-      comments: [
-        comments,
-      ],
-    };
 
     // Generate dependencies
     const mockThreadRepository = new ThreadRepository();
     const mockCommentRepository = new CommentRepository();
 
     // Fake implementation
-    mockThreadRepository.getById = jest.fn()
-      .mockImplementation(() => Promise.resolve({
-        id, title: expectedThread.title, body: expectedThread.body, username: expectedThread.username, created_at: expectedThread.date, deleted_at: '2021-01-01 20:20:10',
-      }));
     mockThreadRepository.verifyDeletedThread = jest.fn()
       .mockImplementation(() => Promise.resolve(true));
-    mockCommentRepository.getByThread = jest.fn()
-      .mockImplementation(() => Promise.resolve([
-        {
-          id: 'user-abc01',
-          created_at: '2021-01-01 20:20:10',
-          username: 'username',
-          content: 'content',
-        },
-      ]));
 
     // Generate use case instance
     const getDetailThreadUseCase = new GetDetailThreadUseCase({
@@ -81,6 +49,7 @@ describe('GetDetailThreadUseCase', () => {
 
     // Action
     await expect(getDetailThreadUseCase.execute(id)).rejects.toThrowError('DETAIL_THREAD.DELETED');
+    expect(mockThreadRepository.verifyDeletedThread).toBeCalledWith(id);
   });
 
   it('should return correct fields', async () => {
@@ -148,6 +117,10 @@ describe('GetDetailThreadUseCase', () => {
     expect(detailThread.comments[0]).toHaveProperty('username');
     expect(detailThread.comments[0]).toHaveProperty('date');
     expect(detailThread.comments[0]).toHaveProperty('likeCount');
+
+    expect(mockThreadRepository.verifyDeletedThread).toBeCalledWith(id);
+    expect(mockThreadRepository.getById).toBeCalledWith(id);
+    expect(mockCommentRepository.getByThread).toBeCalledWith(id);
   });
 
   it('should return deleted comment with note', async () => {
@@ -204,5 +177,8 @@ describe('GetDetailThreadUseCase', () => {
     const detailThread = await getDetailThreadUseCase.execute(id);
 
     expect(detailThread.comments[0].content).toBe('**komentar telah dihapus**');
+    expect(mockThreadRepository.verifyDeletedThread).toBeCalledWith(id);
+    expect(mockThreadRepository.getById).toBeCalledWith(id);
+    expect(mockCommentRepository.getByThread).toBeCalledWith(id);
   });
 });
